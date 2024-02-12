7db1e787d22340d6c32bb2c2bde279c1cb52fb5d
noahmarro <naoriglo@gmail.com>
2024-02-12 16:31:38 +0100

    Two Sets

--- introductoryProblems/TwoSets.java
+++ introductoryProblems/TwoSets.java
@@ -1,7 +1,7 @@
 import java.io.*;
 
 public class TwoSets {
-  private static int n;
+  static int n;
 
   public static void main(String[] args) throws IOException {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -9,18 +9,18 @@ public class TwoSets {
     if (n * (n + 1) / 2 % 2 == 0) {
       System.out.println("YES");
       if (n % 2 == 0) {
-        print(new StringBuilder(), 1, n);
-        print(new StringBuilder(), 2, n - 1);
+        o(new StringBuilder(), 1, n);
+        o(new StringBuilder(), 2, n - 1);
       } else {
-        print(new StringBuilder("1 2 "), 4, n);
-        print(new StringBuilder("3 "), 5, n - 1);
+        o(new StringBuilder("1 2 "), 4, n);
+        o(new StringBuilder("3 "), 5, n - 1);
       }
     } else {
       System.out.println("NO");
     }
   }
 
-  private static void print(StringBuilder s, int l, int r) {
+  static void o(StringBuilder s, int l, int r) {
     System.out.println(n / 2 + (s.length() == 4 ? 1 : 0));
     while (l < r) {
       s.append(l);
