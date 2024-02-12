1c0397db222556291527da05ace2771970096be8
noahmarro <naoriglo@gmail.com>
2024-02-12 13:40:53 +0100

    Weird Algorithm

--- introductoryProblems/WeirdAlgorithm.java
+++ introductoryProblems/WeirdAlgorithm.java
@@ -1,11 +1,10 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class WeirdAlgorithm {
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    long n = input.nextLong();
-    input.close();
-    while (n >= 1) {
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    long n = Long.parseLong(reader.readLine());
+    while (n > 0) {
       System.out.print(n + " ");
       n = n % 2 == 0 ? n / 2 : n > 1 ? n * 3 + 1 : 0;
     }
